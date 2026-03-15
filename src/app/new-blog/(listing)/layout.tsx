9eb6ee04127b4880c280e45acd04f8d92ffe8bc5
Adam Bryndza <bryndza.ad@gmail.com>
2026-03-15 23:03:15 +0100

    added basic metadata

--- src/app/new-blog/(listing)/layout.tsx
+++ src/app/new-blog/(listing)/layout.tsx
@@ -1,5 +1,11 @@
 import { ListingLayout } from "@/features/blog";
 
+export const metadata = {
+  title: "Notatki Junior Software Engineer'a - Blog",
+  description:
+    "Moje osobiste notatki z nauki. Rozkładam trudne tematy z IT na czynniki pierwsze, żeby samemu je lepiej zrozumieć.",
+};
+
 export default function Listing({ children }: { children: React.ReactNode }) {
   return <ListingLayout>{children}</ListingLayout>;
 }
