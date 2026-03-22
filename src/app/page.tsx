63edee03677810d5a76ffea5f0eefa414b592225
Adam Bryndza <bryndza.ad@gmail.com>
2026-03-22 22:28:32 +0100

    add3e metadata to Home component and updated HomePage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { HomePage } from "@/shared/pages";
 
+export const metadata = {
+  title: "Adam Bryndza - Software Engineer",
+  description: "Strona w budowie",
+};
+
 export default function Home() {
   return <HomePage />;
 }
--- src/shared/pages/home.page.tsx
+++ src/shared/pages/home.page.tsx
@@ -1,12 +1,13 @@
+import { urls } from "@/features/blog/urls";
 import { ButtonLink } from "@/shared/components/ui";
 
 export function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center grow text-center">
-      <h1 className="text-4xl laptop:text-6xl font-bold mb-8 laptop:mb-15 text-foreground">
+      <h1 className="text-3xl tablet:text-4xl laptop:text-6xl font-bold mb-8 laptop:mb-15 text-foreground">
         Hello, world!
       </h1>
-      <ButtonLink href="/new-blog" className="desktop:text-xl">
+      <ButtonLink href={urls.main()} className="desktop:text-xl">
         Zobacz bloga
       </ButtonLink>
     </div>
